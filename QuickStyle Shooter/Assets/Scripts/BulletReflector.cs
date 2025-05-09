18b1cdb967a17e79bf8d7c83915d5649ceca8db8
Foxrigona <ethanhammah.ib@gmail.com>
2025-05-09 14:44:02 -0400

    Bullet reflection

---
+++ QuickStyle Shooter/Assets/Scripts/BulletReflector.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent (typeof(Collider2D))]
+public class BulletReflector : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float moveSpeed;
+    public void Start()
+    {
+        rb.velocity = transform.up * this.moveSpeed;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        float collisionAngle = 90 + Vector2.SignedAngle(-transform.up, collision.contacts[0].normal);
+        Debug.Log(collisionAngle);
+        this.transform.Rotate(0, 0, collisionAngle * 2);
+        rb.velocity = transform.up * this.moveSpeed;
+    }
+}
