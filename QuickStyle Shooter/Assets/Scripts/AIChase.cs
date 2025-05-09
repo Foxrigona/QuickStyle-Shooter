7094da1fc1f9dd10dc04db77fe89ae34871838db
BagOfPaperClips <samrgenovese@gmail.com>
2025-05-09 16:40:37 -0400

    Background and AI

--- QuickStyle Shooter/Assets/Scripts/AIChase.cs
+++ QuickStyle Shooter/Assets/Scripts/AIChase.cs
@@ -6,8 +6,7 @@ public class AIChase : MonoBehaviour
 {
     public GameObject player;
     public float speed; // Best when 1
-    public float distanceToStartChase = 4;  //Start chase 15
-    public float inDistanceExplode = 3;  //Best when 3
+    public float inDistanceExplode = 3;  //Best when 2
 
     private float distance;
 
@@ -20,14 +19,6 @@ public class AIChase : MonoBehaviour
         Vector2 direction = player.transform.position - transform.position;
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-       
-
-        if(distance < distanceToStartChase)
-        {
-           transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-           transform.rotation = Quaternion.Euler(Vector3.forward * angle);
-        }
 
         if(distance < inDistanceExplode)
         {
